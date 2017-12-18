b2fe353977a7405b9f1744781911fcd4f7ae3931
bykrab <egor.bukraba@gmail.com>
2017-12-18 09:47:59 +0300

    add rinkeby token submission after facebook share

--- assets/js/get_test_tokens.js
+++ assets/js/get_test_tokens.js
@@ -9,40 +9,86 @@
         },
 
         init: function(web3, address) {
+            const self = this;
             this.web3 = web3;
             this.address = address;
 
-            twttr.widgets.createShareButton(
-                '/',
-                document.getElementById('get-test-tokens-twitter-button'),
-                {
-                    text: `Requesting faucet funds into ${address} on the #Rinkeby #Ethereum test network.'\n https://plasma.bankex.com/ test text`
-                }
-            );
+            // twttr.ready((twttr) => {
+            //     twttr.widgets.createShareButton(
+            //         '/',
+            //         document.getElementById('get-test-tokens-twitter-button'),
+            //         {
+            //             url: 'https://plasma.bankex.com/',
+            //             text: `Requesting faucet funds into ${address} on the #Rinkeby #Ethereum test network.'\n https://plasma.bankex.com/ test text`
+            //         }
+            //     );
+            //     twttr.events.bind(
+            //         'click',
+            //         (event) => {
+            //             console.log(event);
+            //             // this.giveMeTokens('twitter');
+            //         }
+            //     );
+            // });
 
-            $('#get-test-tokens-facebook-button').on('click', function() {
+            $('#get-free-tokens').on('click', () => {
+                this.showFacebookShareModal()
+            });
+
+            this.getTokensButton.on('click', this.showSocialNetworksPopup);
+        },
+
+        showFacebookShareModal: function() {
+            console.log(this);
+            FB.login(() => {
                 FB.ui(
                     {
                         method: 'share',
-                        href: 'https://plasma.bankex.com/',
+                        href: 'https://plasma.bankex.com/'
                     },
                     // callback
-                    function(response) {
+                    (response) => {
                         if (response && !response.error_message) {
-                            alert('Posting completed. Your ETH in a way');
-                        } else {
-                            alert('Error while posting.');
+                            const userID = FB.getUserID();
+                            this.giveMeTokens('fb', userID);
                         }
                     }
                 );
+            })
+        },
+
+        giveMeTokens: function(networkName, userId) {
+            //should be replaced on correct one
+            const url = 'http://localhost:3000/web3/rinkeby/tokensPlease';
+            var data = new FormData();
+            data.append('json', {
+                wallet: this.address,
+                social: {
+                    name: networkName,
+                    userId: userId
+                }
             });
 
+            $.ajax({
+                url: url,
+                type: 'JSON',
+                method: 'POST',
+                data: {
+                    wallet: this.address,
+                    networkName: networkName,
+                    userId: userId
+                }
+            })
+                .success(function(res) {
+                    if (res.success) {
+                        alert('Your tokens are coming. Transaction hash: ' + res.transactionHash);
+                    } else {
+                        // alert(res.message);
+                    }
+                });
 
-
-
-
-            this.getTokensButton.on('click', this.showSocialNetworksPopup.bind(this));
         }
+
     };
 
 
